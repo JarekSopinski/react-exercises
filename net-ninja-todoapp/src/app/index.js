3858769c8d4e692cedf940cb8241f3df2c8baa99
JarekSopinski <jarek.sopinski@gmail.com>
2018-02-22 15:00:10 +0100

    net-ninja-todoapp lesson3

--- net-ninja-todoapp/src/app/index.js
+++ net-ninja-todoapp/src/app/index.js
@@ -1,1 +1,22 @@
-alert('It Woooorks');
+const React = require('react');
+const ReactDOM = require('react-dom');
+const createReactClass = require('create-react-class');
+
+//Create component
+
+const TodoComponent = createReactClass({
+
+    render: function () {
+
+        return(
+            <div>
+                <h1>Hello</h1>
+                <p>World</p>
+            </div>
+        );
+    }
+
+});
+
+// put component into html page
+ReactDOM.render(<TodoComponent/>, document.getElementById('todo-wrapper'));

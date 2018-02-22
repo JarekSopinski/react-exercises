2463013d35643eaba069044fc8d82381e00c2723
JarekSopinski <jarek.sopinski@gmail.com>
2018-02-22 16:06:56 +0100

    net-ninja-todoapp lesson5.5 changing state

--- net-ninja-todoapp/src/app/index.js
+++ net-ninja-todoapp/src/app/index.js
@@ -8,15 +8,23 @@ const TodoComponent = createReactClass({
 
     getInitialState: function () {
         return {
-            todos: ["wash up", "eat some cheese", "take a nap"]
+            todos: ["wash up", "eat some cheese", "take a nap"],
+            age: 30,
         }
     },
 
     render: function () {
 
+        const ager = setTimeout(function () {
+            this.setState({
+                age: 35,
+            })
+        }.bind(this), 5000);
+
         return(
             <div id="todo-list">
                 <p>The busiest people have the most leisure!</p>
+                <p>{this.state.age}</p>
                 <ul>
                     <li>{this.state.todos[0]}</li>
                     <li>{this.state.todos[1]}</li>

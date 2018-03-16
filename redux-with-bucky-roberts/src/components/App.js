9b82b1f5e866c6c898e729e8a81236364d9bfc78
JarekSopinski <jarek.sopinski@gmail.com>
2018-03-16 22:25:38 +0100

    Sucesfully passed data from store to component

--- redux-with-bucky-roberts/src/components/App.js
+++ redux-with-bucky-roberts/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import UserList from '../containers/UserList'
 
 class App extends Component {
     render() {
         return (
             <div>
                 <h2>Username list:</h2>
+                <UserList/>
                 <br/>
                 <h2>Username details:</h2>
             </div>
